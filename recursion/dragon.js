e2c81d027b8634f38a28e0c5513db08871b858fa
Bhavleen <singhbhavleen3@gmail.com>
2021-03-17 20:10:20 +0530

    Recursion with Dragon Example

---
+++ recursion/dragon.js
@@ -0,0 +1,13 @@
+const dragon = (numbers) => {
+  numbers[0] % 2 === 0
+    ? result.push("EVEN")
+    : result.push("ODD");
+  numbers.shift();
+
+  if (numbers.length === 0) return;
+  else dragon(numbers);
+};
+
+let result = [];
+dragon([3142, 5798, 6550, 5914]);
+console.log(result);
