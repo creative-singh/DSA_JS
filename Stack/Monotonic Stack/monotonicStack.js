3f3446e956fa8ad67e5bc8aa5f58216ea5ca0dd3
Bhavleen Singh <50984385+creative-singh@users.noreply.github.com>
2025-06-13 14:53:39 +0530

    Updated monotonicStack.js

---
+++ Stack/Monotonic Stack/monotonicStack.js
@@ -0,0 +1,42 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  print() {
+    console.log(this.items.toString());
+  }
+}
+
+const stack = new Stack();
+console.log(stack.isEmpty());
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.print();
+console.log(stack.size());
+
+console.log(stack.pop(3));
+stack.print();
+console.log(stack.peek());
